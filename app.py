980064154327cb21b4f7ef2118796159534ca6db
Felipe Perez <felipe@chichasov.es>
2015-01-08 00:36:39 +0100

    Documentation

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import flask 
 import string, random
 
-app = flask.Flask(__name__)
-app.config['MAX_CONTENT_LENGTH'] = 10 * 1024 * 1024 #10MiB
+app = flask.Flask(__name__) #Initialize our application
+app.config['MAX_CONTENT_LENGTH'] = 10 * 1024 * 1024 #Set the upload limit to 10MiB
 
-def genHash(seed):
-    base = string.ascii_lowercase+string.digits
+def genHash(seed): #Generate five letter filenames for our files
+    base = string.ascii_lowercase+string.digits 
     random.seed(seed)
     hash_value = ""
     for i in range(5):
@@ -16,10 +16,10 @@ def genHash(seed):
 @app.route('/', methods=['GET', 'POST'])
 def index():
 	if flask.request.method == 'POST':
-		print 'test'
 		"""
-			NOTE
-			SANITIZE INPUT HERE. ON THE TODO LIST.
+			File upload happens here.
+			We get your filename and convert it to our hash with your extension.
+			Then we redirect to the file itself.
 		"""
 		f = flask.request.files['file']
 		extension = f.filename.split('.')[-1]
@@ -28,11 +28,13 @@ def index():
 		return flask.redirect(flask.url_for('getFile', filename=filename))
 	else:
 		return flask.render_template('index.html')
+		#Return the index to the client.
 
 @app.route('/<filename>/')
-def getFile(filename):
+def getFile(filename): #File delivery to the client
 	return flask.send_from_directory('static/files', filename)
+	#Gets the file 'filename' from the directory /static/files/
 
 
 if __name__ == '__main__':
-	app.run() 
+	app.run() #Run our app.
