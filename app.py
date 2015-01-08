955baf1c0e8824f08a96e48a350ee3cd0e3c5493
Felipe Perez <felipe@chichasov.es>
2015-01-08 15:31:25 +0100

    New filesystem(ho.st/randgen/original_filena.me) and some error messages.

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 import flask 
-import string, random
+import string, random, hashlib, os
+from werkzeug import secure_filename
+
+#Load config file
+config = {}
+with open('config.ini', 'r') as file:
+	for line in file.read().splitlines():
+		line = line.split('==')
+		config[line[0]] = line[1]
 
 app = flask.Flask(__name__) #Initialize our application
 app.config['MAX_CONTENT_LENGTH'] = 10 * 1024 * 1024 #Set the upload limit to 10MiB
+app.secret_key = config['SECRET_KEY']
 
 def genHash(seed): #Generate five letter filenames for our files
     base = string.ascii_lowercase+string.digits 
@@ -22,20 +31,40 @@ def index():
 			Then we redirect to the file itself.
 		"""
 		f = flask.request.files['file']
-		extension = f.filename.split('.')[-1]
-		filename = genHash(f.filename) + '.' + extension
-		f.save('static/files/%s' % filename)
-		print 'Uploaded file \'%s\'' % filename #Log what file was uploaded
-		return flask.redirect(flask.url_for('getFile', filename=filename))
+		
+		hasher = hashlib.md5() 		
+		buf = f.read()		   		
+		f.seek(0) #Set cursor back to position 0 so we can read it again in the save function.
+									# We hash the file to get its filename.	   		
+									# So that we can upload two different images with the same filename,
+		hasher.update(buf)	   		# But not two same images with different filenames.
+		dirname = genHash(hasher.hexdigest())
+		if not os.path.exists("static/files/%s" % dirname): # Check if the folder already exists
+			os.mkdir('static/files/%s' % dirname) #Make it
+			f.save('static/files/%s/%s' % (dirname, secure_filename(f.filename)))
+			print 'Uploaded file \'%s\'' % secure_filename(f.filename) #Log what file was uploaded
+			return flask.redirect(flask.url_for('getFile', dirname=dirname,filename=secure_filename(f.filename)))
+		else:
+			flask.flash('File already exists in %s!' % dirname) #Display a message for the user.
+			return flask.redirect(flask.url_for('index'))
 	else:
 		return flask.render_template('index.html')
 		#Return the index to the client.
 
-@app.route('/<filename>/')
-def getFile(filename): #File delivery to the client
-	return flask.send_from_directory('static/files', filename)
-	#Gets the file 'filename' from the directory /static/files/
+@app.route('/<dirname>/')
+@app.route('/<dirname>/<filename>/')
+def getFile(dirname, filename=None): #File delivery to the client
+	if filename: #Dir and filename is provided
+		return flask.send_from_directory('static/files/%s' % (dirname), filename) #Gets the file 'filename' from the directory /static/files/
+	elif not filename: #Filename is absent - we get it for them.
+		if os.path.exists('static/files/%s' % dirname): #Does it even exist?
+			files = os.listdir('static/files/%s' % dirname)
+			if files: #Check if there's any file in the directory.
+				return flask.send_from_directory('static/files/%s' % (dirname), files[0])
+		else:
+			flask.abort(404)
 
 
 if __name__ == '__main__':
+	app.debug = True
 	app.run() #Run our app.
