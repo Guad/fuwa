469ac7ab90eb1f8a30efa7b3f4569b08490c06f6
Phillip <felipe@chichasov.es>
2015-06-18 03:14:13 +0200

    Added async upload

--- static/upload.js
+++ static/upload.js
@@ -10,71 +10,92 @@ $(document).ready(function()
 		if(files.length === 0) {
 			alertCard('No files');
 		} else {
-			for (var i = 0; i < files.length; i++) {
-				var file = files[i];
-				var id = i;
-				if(file.size > 10 * 1024 * 1024)
-				{
-					alertCard('File too large!');
-					continue;
-				}
-				var formData = new FormData();
-
-				formData.append('file[]', file, file.name);
-
-				fileCard(file.name, id);
-
-				var request = $.ajax({
-					url: '/js',
-					type: 'POST',
-					async:false,
-					data: formData,
-					processData: false,
-					contentType: false,
-					xhr: function() {
-						var myXhr = $.ajaxSettings.xhr();
-		            	if(myXhr.upload){
-		                	myXhr.upload.addEventListener('progress',function(e){
-		                		if(e.lengthComputable){
-							        var max = e.total;
-							        var current = e.loaded;
-							        var Percentage = (current * 100)/max;
-							        var divid = '#file' + id + ' > div';
-							        $(divid).width(Percentage + '%');
-							        console.log(divid);
-							        console.log(Percentage + '%');
-    							}  
-		                	}, false);
-		                }
-		            	return myXhr;
-					},
-					complete: function( jqXHR, textStatus) {
-						//alert(textStatus);
-						console.log(jqXHR);
-						var resp = jqXHR.responseText;
-						var code = resp.split(":");
-						
-						if(code[0] === 'success') {
-							$('<a target="_blank" href="' + code[1] +'">uploaded to "' + code[2] + '"</a>').insertAfter('#file' + id);
-						}
-						else if(code[0] === 'exists') {
-							$('<a target="_blank" href="' + code[1] + '">The file already exists.</a>').insertAfter('#file' + id);
-						}
-						else if(code[0] === 'error') {
-							$('<p>Invalid filename.</p>').insertAfter('#file' + id);
-						}						
-						$('#file' + id).fadeOut(1000);
-					},
-					error: function(jqXHR, textStatus, errorThrown) {
-						//alert(errorThrown);
-						console.log(errorThrown);
-						console.log(textStatus);
-					}
-				});
-			}
+			processFilesRecursively(files)
 		}
 	}
 });
+
+var gCounter = 0;
+var total = 0;
+function processFilesRecursively(fileArray)
+{
+	if(gCounter >= fileArray.length) {
+		gCounter = 0;
+		total += 1
+		return 0;
+	} else {
+	var id = gCounter + total;
+	var file = fileArray[gCounter];
+	if(file.size > 10 * 1024 * 1024)
+	{
+		//alertCard('File too large!');
+		fileAlertCard(file.name, 'File too large!');
+		gCounter += 1;
+		total += 1;
+		processFilesRecursively(fileArray);
+		return 0;
+	}
+
+	var formData = new FormData();
+
+	formData.append('file[]', file, file.name);
+	fileCard(file.name, id);
+
+	var request = $.ajax({
+		url: '/js',
+		type: 'POST',
+		async: true,
+		data: formData,
+		processData: false,
+		contentType: false,
+		xhr: function() {
+			var myXhr = $.ajaxSettings.xhr();
+        	if(myXhr.upload){
+            	myXhr.upload.addEventListener('progress',function(e){
+            		if(e.lengthComputable){
+				        var max = e.total;
+				        var current = e.loaded;
+				        var Percentage = (current * 100)/max;
+				        var divid = '#file' + id + ' > div';
+				        $(divid).width(Percentage + '%');
+				        console.log(divid);
+				        console.log(Percentage + '%');
+					}  
+            	}, false);
+            }
+        	return myXhr;
+		},
+		complete: function( jqXHR, textStatus) {
+			//alert(textStatus);
+			console.log(jqXHR);
+			var resp = jqXHR.responseText;
+			var code = resp.split(":");
+			
+			if(code[0] === 'success') {
+				$('<a target="_blank" href="' + code[1] +'">uploaded to "' + code[2] + '"</a>').insertAfter('#file' + id);
+			}
+			else if(code[0] === 'exists') {
+				$('<a target="_blank" href="' + code[1] + '">The file already exists.</a>').insertAfter('#file' + id);
+			}
+			else if(code[0] === 'error') {
+				$('<p>Invalid filename.</p>').insertAfter('#file' + id);
+			}						
+			$('#file' + id).fadeOut(1000);
+			gCounter += 1;
+			total += 1;
+			processFilesRecursively(fileArray);
+		},
+		error: function(jqXHR, textStatus, errorThrown) {
+			//alert(errorThrown);
+			console.log(errorThrown);
+			console.log(textStatus);
+			gCounter += 1;
+			total += 1;
+			processFilesRecursively(fileArray);
+		}
+	});
+	}
+}
 
 function fileCard(filename, id) {
 	var start = '<div class="row card-out"><div class="col s12 l4 offset-l4 m8 offset-m2"><div class="card ';
@@ -108,6 +129,34 @@ function fileCard(filename, id) {
 	div.fadeIn(1000);
 }
 
+function fileAlertCard(filename, text) {
+	var start = '<div class="row card-out"><div class="col s12 l4 offset-l4 m8 offset-m2"><div class="card ';
+	var color = 'blue-grey ';
+	var inter = 'darken-1 z-depth-2"><div class="card-content white-text">'
+	var head = '<h5 class="truncate">';
+	var name =  filename;
+	var action = '</h5></div><div class="card-action">';
+	var progress2 = '<a>' + text + '</a></div>';
+	var actionend = '</div>';
+	var inter2 = '';
+	var end = '</div></div></div>';
+
+	var div = $(start +
+				color +
+				inter +
+				head +
+				name +
+				action +
+				progress2 +
+				actionend +
+				inter2 +
+				end
+				);
+	
+	div.hide();
+	div.insertAfter('#afterthis');
+	div.fadeIn(1000);
+}
 
 function alertCard(text) {
 	var start = '<div class="row card-out"><div class="col s12 l4 offset-l4 m8 offset-m2"><div class="card ';
